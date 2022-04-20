adc26c8ffa549aa56e6b7475a0c324f84f77eeba
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-20 16:45:26 -0300

    add component footer and create ContainerInfo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,39 @@
 import React from 'react'
 
-import { Container } from './styles'
+import { Container, ContainerInfo } from './styles'
 
-import { Header, Banner } from '../../components'
+import { Header, Banner, Footer } from '../../components'
 
 function Home () {
   return (
     <Container>
       <Header />
       <Banner />
+      <ContainerInfo>
+        <h1> Sobre Nós </h1>
+        <p>
+          {' '}
+          A Internet e os serviços por ela oferecidos já fazem parte de nossas
+          vidas e vieram para ficar. É sabido que as empresas que não estão
+          presentes na Web de uma forma planejada e eficiente, deixaram de ser
+          competitivas diante da concorrência do mercado globalizado. Sabemos
+          que o modelo de comércio tradicional (de balcão) continuará a existir
+          sempre, mas reflitamos um pouco: no mundo atual, onde a falta de tempo
+          é uma constante na vida de todas as pessoas, a tendência natural do
+          cliente será a de privilegiar cada vez mais quem lhe oferecer produtos
+          e serviços 24 horas por dia, mantendo-o na comodidade e segurança de
+          sua empresa ou de seu lar, livre dos congestionamentos, da violência
+          urbana, das filas e de outros inconvenientes da vida moderna.
+           A Internet proporciona tudo isso e a SuperCarros.com.br quer ampliar ainda
+          mais suas fronteiras digitais. Somos uma equipe altamente qualificada
+          composta por dezenas de profissionais nas áreas de Programação,
+          Design, Prospecção, Atendimento ao Cliente, Web Business, Marketing e
+          Gestão Empresarial. Nosso objetivo é levar a praticidade para dentro
+          de sua casa ou trabalho, para que seus sonhos e projetos se realizem
+          mais rápido e da melhor forma possível.
+        </p>
+      </ContainerInfo>
+      <Footer />
     </Container>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,27 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    width: 100vw;
-    min-height: 100vh;
-    background-color:white;
+  width: 100%;
+  min-height: 100vh;
+  background-color: white;
+`
+export const ContainerInfo = styled.div`
+  display: flex;
+  background-color: #eef3f8;
+  flex-flow: column wrap;
+  padding-top: 40px;
+  margin: 0 auto;
+
+  h1 {
+  color: rgb(0, 0, 0);
+  font-size: 40px;
+  text-align: center;
+}
+
+p{
+    font-size: 20px;
+  color: rgb(0, 0, 0);
+  text-align: center;
+  padding: 20px 50px 50px 50px;
+}
 `

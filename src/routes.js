98ce598124a8ede6af631bb7211bf4c5319b13fb
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-21 12:32:15 -0300

    add cars in routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
-import Home from './pages/Home'
+import { Home, Cars } from './pages'
 
 function AppRoutes () {
   return (
     <Router>
       <Switch>
-        <Route path='/' component={Home} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/carros' component={Cars} />
       </Switch>
     </Router>
   )

5a3c96ca0ae34aa2616f634043de0c15f217198f
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-22 19:06:52 -0300

    add image logo and link

--- src/components/SideMenuAdmin/index.js
+++ src/components/SideMenuAdmin/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { useHistory } from 'react-router-dom'
+
 import { Container, ItemContainer, ListLink } from './styles'
 import listLinks from './menu-list'
+import Logo from '../../assets/logo_super_carros.png'
 
 export function SideMenuAdmin ({ path }) {
+  const {
+    push,
+    location: { pathname }
+  } = useHistory()
   return (
     <Container>
       <hr></hr>
@@ -15,8 +22,12 @@ export function SideMenuAdmin ({ path }) {
         </ItemContainer>
       ))}
       <hr></hr>
-      <ItemContainer style={{ position: 'fixed', bottom: '30px' }}>
-      </ItemContainer>
+      <ItemContainer
+        style={{ position: 'fixed', bottom: '30px' }}
+      ></ItemContainer>
+      <button onClick={() => push('/carros')}>
+        <img src={Logo} alt='Logo' />
+      </button>
     </Container>
   )
 }

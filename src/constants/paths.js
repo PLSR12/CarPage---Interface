73a705235a40027cde079ab7772e527e267c1aa6
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-24 16:21:16 -0300

    add new paths for admin motors

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,7 +1,11 @@
 const paths = {
     Cars: '/admin-carros',
     EditCar: '/editar-carro',
-    NewCar: '/novo-carro'
+    NewCar: '/novo-carro',
+
+    Motors:'/admin-motos',
+    EditMotor: '/editar-moto', 
+    NewMotor: '/nova-moto'
 }
 
 export  default paths

0e0cc53a6c43533e90c653a7c6820ea28a4e5fa5
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-25 18:31:50 -0300

    changes MUI Icons

--- src/components/SideMenuAdminMotors/menu-list.js
+++ src/components/SideMenuAdminMotors/menu-list.js
@@ -1,19 +1,26 @@
-import DirectionsCarIcon from '@mui/icons-material/DirectionsCar'
 import AddCircleIcon from '@mui/icons-material/AddCircle'
 import paths from '../../constants/paths'
+import TwoWheelerIcon from '@mui/icons-material/TwoWheeler'
+import BrandingWatermarkIcon from '@mui/icons-material/BrandingWatermark';
 
 const listLinks = [
   {
     id: 1,
     label: 'Listar Motos',
     link: paths.Motors,
-    icon: DirectionsCarIcon
+    icon: TwoWheelerIcon
   },
   {
     id: 2,
     label: 'Nova Moto',
     link: paths.NewMotors,
     icon: AddCircleIcon
+  },
+  {
+    id: 3,
+    label: 'Nova Marca',
+    link: paths.NewBrand,
+    icon: BrandingWatermarkIcon
   }
 ]
 

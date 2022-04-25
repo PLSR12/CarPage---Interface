7ecfc9e85e56d9e58346c70926b541b405ee2366
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-25 12:12:21 -0300

    add toastContainer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ToastContainer } from 'react-toastify'
 
 import GlobalStyles from './styles/GlobalStyles'
 import AppRoutes from './routes'
@@ -7,6 +8,7 @@ import AppRoutes from './routes'
 ReactDOM.render(
   <>
     <AppRoutes />
+    <ToastContainer autoClose={2500} theme='colored' />
     <GlobalStyles />
   </>,
   document.getElementById('root')

7701cf98050d1e456bf751260366f5c1195458f6
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-26 14:06:20 -0300

    create route for trucks

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
-import { Home, Cars, Motorcycles, Admin, AdminMotors } from './pages'
+import { Home, Cars, Motorcycles, Admin, AdminMotors, Trucks } from './pages'
 import paths from './constants/paths'
 
 function AppRoutes () {
@@ -11,6 +11,7 @@ function AppRoutes () {
         <Route exact path='/' component={Home} />
         <Route exact path='/motos' component={Motorcycles} />
         <Route exact path='/carros' component={Cars} />
+        <Route exact path='/caminhoes' component={Trucks} />
         <Route exact path={paths.Cars} component={Admin} />
         <Route exact path={paths.NewCar} component={Admin} />
         <Route exact path={paths.EditCar} component={Admin} />

fa126560dd8ed0102b98aa48fa2b96f5e4b5fd2d
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-28 16:22:59 -0300

    updated class

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,7 +15,7 @@ export function Footer () {
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='currentColor'
-                class='bi bi-geo-alt-fill'
+                className='bi bi-geo-alt-fill'
                 viewBox='0 0 16 16'
               >
                 <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
@@ -36,7 +36,7 @@ export function Footer () {
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   fill='currentColor'
-                  class='bi bi-cursor-fill'
+                  className='bi bi-cursor-fill'
                   viewBox='0 0 16 16'
                 >
                   <path d='M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z' />
@@ -51,7 +51,7 @@ export function Footer () {
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
                       fill='currentColor'
-                      class='bi bi-facebook'
+                      className='bi bi-facebook'
                       viewBox='0 0 16 16'
                     >
                       <path d='M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z' />
@@ -63,7 +63,7 @@ export function Footer () {
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
                       fill='currentColor'
-                      class='bi bi-instagram'
+                      className='bi bi-instagram'
                       viewBox='0 0 16 16'
                       href='https://www.instagram.com/sereiamimi/?igshid=6crvbbcokh1y/'
                     >
